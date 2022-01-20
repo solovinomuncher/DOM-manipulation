e063dad9e0f889969b4d63b85b261b0542cd92bb
Erick Esqueda <eriesqu19@gmail.com>
2022-01-20 11:20:48 -0800

    added simple DOM manipulation
    practiced simple DOM manipulation, and two ways to handle onclick events: with onclick method, and with addEventListener('click',...) method.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// ====================================================
+// simple DOM manipulation: changing value of element
+// ====================================================
+
 let docObj = document.querySelector('.header-title');
 
 function eventHandler(e) {
@@ -14,3 +18,26 @@ function eventHandler2(e) {
 
 docObj.addEventListener('mouseenter', eventHandler);
 docObj.addEventListener('click', eventHandler2);
+
+// ====================================================
+// simple DOM manipulation: handling onclick events
+// ====================================================
+
+let mainButton = document.querySelector('.main-button');
+
+// with this way of handling clicks, only one onclick handler can be assigned
+// to an object at a time. addEventListener is more flexible in this regard.
+// mainButton.onclick = function() {
+//     console.log('onclick event');
+//     this.textContent = 'Clicked!';
+// }
+
+function eventHandler3(e) {
+    console.log('onclick event');
+    this.textContent = 'Clicked!';
+
+    // reloads the current URL, like the Refresh button
+    // location.reload();
+}
+
+mainButton.addEventListener('click', eventHandler3);
